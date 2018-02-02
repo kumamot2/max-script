f70a6cfc93ab0a0b153200f8d30e19b8237e2c4e
Maika Kumamoto <maika.kumamoto@gmail.com>
2018-02-02 11:13:41 -0600

    adding in comments

--- wordcount.sh
+++ wordcount.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# get rid of comment lines
+# search for word (case insensitive, whole words only)
+#translate spaces to newlines
 grep -v "^#" $2 | grep -i -w $1 | tr ' ' '\n' | grep -i -w $1 | wc -l
 
 
